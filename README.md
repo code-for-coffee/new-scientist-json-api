57c86e3cf53ca351b0ee352d17346f122a773092
code-for-coffee <jtraverjr@gmail.com>
2016-07-14 11:33:29 -0500

    added all xml endpoints

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# New Scientist JSON API
-Simple JSON API wrapper for New Scientist's XML feeds. Pulls feed data from https://www.newscientist.com/rss-feeds/.
+# new-scientist-json-api
+Simple JSON API wrapper for New Scientist's XML feeds.
 
+## Endpoints
 
+* `/` - All articles.
+* `/news` - News articles.
+* `/features` - New Scientist features.
+* `/math-pyshics` - Math & Physics.
+* `/tech` - Technology.
+* `/space` - Space exploration.
+* `/life` - Life sciences.
+* `/environment` - Environmental Sciences
+* `/health` - Health Sciences
+* `/humans` - Anthro-related topics
--- app.rb
+++ app.rb
@@ -1,7 +1,49 @@
 require 'bundler'
 Bundler.require
 
-get '/' do
-  @xml = HTTParty.get('http://feeds.newscientist.com/')
+
+
+def xml_feed_to_json(url)
+  @xml = HTTParty.get(url)
   Crack::XML.parse(@xml).to_json
+end
+
+get '/' do
+  xml_feed_to_json('http://feeds.newscientist.com/')
+end
+
+get '/news' do
+  xml_feed_to_json('http://feeds.newscientist.com/science-news')
+end
+
+get '/features' do
+  xml_feed_to_json('http://feeds.newscientist.com/features')
+end
+
+get '/math-pyshics' do
+  xml_feed_to_json('http://feeds.newscientist.com/physics-math')
+end
+
+get '/tech' do
+  xml_feed_to_json('http://feeds.newscientist.com/tech')
+end
+
+get '/space' do
+  xml_feed_to_json('http://feeds.newscientist.com/space')
+end
+
+get '/life' do
+  xml_feed_to_json('http://feeds.newscientist.com/life')
+end
+
+get '/environment' do
+  xml_feed_to_json('http://feeds.newscientist.com/environment')
+end
+
+get '/health' do
+  xml_feed_to_json('http://feeds.newscientist.com/health')
+end
+
+get '/humans' do
+  xml_feed_to_json('http://feeds.newscientist.com/humans')
 end
